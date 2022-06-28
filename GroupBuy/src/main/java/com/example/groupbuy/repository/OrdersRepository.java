db5232069a8e07324d1b0709054b2ae990cb9de3
dongyunpeng <dongyunpeng@sjtu.edu.cn>
2022-06-28 15:33:25 +0800

    task1:add config

--- GroupBuy/src/main/java/com/example/groupbuy/repository/OrdersRepository.java
+++ GroupBuy/src/main/java/com/example/groupbuy/repository/OrdersRepository.java
@@ -1,4 +1,17 @@
 package com.example.groupbuy.repository;
 
-public class OrdersRepository {
+
+import com.example.groupbuy.entity.Orders;
+import com.example.groupbuy.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OrdersRepository extends JpaRepository<Orders,Integer>, JpaSpecificationExecutor<Orders> {
+
+    List<Orders> findByUser(User user);
+
 }
--- GroupBuy/src/main/java/com/example/groupbuy/repository/UsersRepository.java
+++ GroupBuy/src/main/java/com/example/groupbuy/repository/UsersRepository.java
@@ -1,4 +1,7 @@
 package com.example.groupbuy.repository;
 
+
+
+
 public class UsersRepository {
 }
